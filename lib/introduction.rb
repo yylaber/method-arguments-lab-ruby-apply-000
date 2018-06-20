edafa3db6a36a133da0d2b18ae4c51b0786ebee8
Yosef Levertov <yylaber@gmail.com>
2018-06-20 18:20:17 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is "  + name + "."
+end
+# puts "rgr"
+puts introduction("Josh")
+def introduction_with_language(name, lang)
+  puts "Hi, my name is " + name + " and I am learning to program in " + lang + "."
+end
+puts introduction_with_language("Josh", "Ember.js")
