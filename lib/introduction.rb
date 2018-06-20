0e7ef7bd51e0b247379dad72dfd79c8cfccbcc1e
Yosef Levertov <yylaber@gmail.com>
2018-06-20 18:17:04 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
 def introduction(name)
   puts "Hi, my name is "  + name
 end
-puts "rgr"
-puts introduction("Yosef")
+# puts "rgr"
+puts introduction("Josh")
 def introduction_with_language(name, lang)
   puts name + lang
 end
