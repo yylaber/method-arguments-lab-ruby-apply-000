65608c4181ead425584e399f445660cbb5397654
Yosef Levertov <yylaber@gmail.com>
2018-06-20 18:19:18 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ end
 # puts "rgr"
 puts introduction("Josh")
 def introduction_with_language(name, lang)
-  puts Hi, my name is and I am learning to program in .
+  puts Hi, my name is " + name + " and I am learning to program in " + lang + "."
 end
 puts introduction_with_language("Josh", "Ember.js")
